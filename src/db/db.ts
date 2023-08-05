94522d1eca321127fc7942e2eff6836e18cb3531
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-08-05 14:43:43 -0700

    expose user to orchidORM for all that sweet autocompletion goodness.

---
+++ src/db/db.ts
@@ -0,0 +1,13 @@
+import { orchidORM } from "orchid-orm";
+import { config } from "../config";
+import { UserTable } from './tables/user.table';
+
+export const db = orchidORM(
+  {
+    databaseURL: config.dbURL,
+    log: true,
+  },
+  {
+    user: UserTable,
+  }
+);
