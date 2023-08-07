7271e837a9d804a330f1d10186bce4177ef2cbc2
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2023-08-07 12:17:32 -0700

    add jest-stup.ts to setupBeforeEnv in jest.config for rolling back all transactions on test db.

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest",
   detectOpenHandles: true,
+  setupFilesAfterEnv: ["./jest-setup.ts"],
 };
 
 export default jestConfig;
